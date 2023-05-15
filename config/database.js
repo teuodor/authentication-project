6f16ea24aecf1d3425b4395de7928786fabe22fb
Marian Nini <marian.nini@endava.com>
2023-05-15 13:03:46 +0300

    remove testing_mode variable

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  const database = process.env.TESTING_MODE ? process.env.MONGO_URI_TESTING : process.env.MONGO_URI;
+  const database = process.env.NODE_ENV === 'testing' ? process.env.MONGO_URI_TESTING : process.env.MONGO_URI;
 
   mongoose.connect(database, {
     useNewUrlParser: true,
